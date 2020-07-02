2744df16c494dff40715b3d418c62ad99c592e42
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-02 13:59:09 +0300

    Update test.js
    CharLookup finished

--- UnitTesting/CharLookup/tests/test.js
+++ UnitTesting/CharLookup/tests/test.js
@@ -1,6 +1,21 @@
 const expect = require("chai").expect;
 const { lookupChar } = require("../app");
 
-describe("", function () {
-  it("", function () {});
+describe("LookupChar", function () {
+  it("Valid data", function () {
+    expect(lookupChar("string", 1)).to.equal("t");
+  });
+  it("Invalid first parameter => not a string", function () {
+    expect(lookupChar(1, 1)).to.equal(undefined);
+  });
+  it("Invalid second parameter => not a number", function () {
+    expect(lookupChar("string", "1")).to.equal(undefined);
+  });
+  it("Floating point numbers => 3.14 return undefined", function () {
+    expect(lookupChar("string", 3.14)).to.equal(undefined);
+  });
+  it("Incorrect index", function () {
+    expect(lookupChar("string", -1)).to.equal("Incorrect index");
+    expect(lookupChar("string", 6)).to.equal("Incorrect index");
+  });
 });
