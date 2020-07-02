3dc96031a3e465c4078668734d0ef6f7cf9189a4
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-02 14:00:28 +0300

    Update test.js
    EvenOrOdd finished

--- UnitTesting/EvenOrOdd/tests/test.js
+++ UnitTesting/EvenOrOdd/tests/test.js
@@ -1,6 +1,17 @@
 const expect = require("chai").expect;
 const { isOddOrEven } = require("../app");
 
-describe("", function () {
-  it("", function () {});
+describe("Check for a string length: EVEN or ODD", function () {
+  it("non string parameter returns undefined", function () {
+    expect(isOddOrEven(1)).to.equal(undefined, "Invalid data!");
+    expect(isOddOrEven(true)).to.equal(undefined, "Invalid data!");
+    expect(isOddOrEven([])).to.equal(undefined, "Invalid data!");
+    expect(isOddOrEven({})).to.equal(undefined, "Invalid data!");
+  });
+  it("Test should return even", function () {
+    expect(isOddOrEven("Test")).to.equal("even");
+  });
+  it("Hey should return odd", function () {
+    expect(isOddOrEven("Hey")).to.equal("odd");
+  });
 });
