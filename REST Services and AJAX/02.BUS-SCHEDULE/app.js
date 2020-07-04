239901b3714966932a715a251cf92553890f586a
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-04 22:43:04 +0300

    Update app.js
    BUS SCHEDULE FINISHED

--- REST Services and AJAX/02.BUS-SCHEDULE/app.js
+++ REST Services and AJAX/02.BUS-SCHEDULE/app.js
@@ -1,17 +1,42 @@
 function solve() {
+  const info = document.querySelector(".info");
+  const departBtn = document.getElementById("depart");
+  const arriveBtn = document.getElementById("arrive");
+  let nextStop = "depot";
+  let currentStop = "";
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+  function depart() {
+    return fetch(`https://judgetests.firebaseio.com/schedule/${nextStop}.json`)
+      .then((res) => {
+        if (res.ok === false) {
+          throw new Error(`${res.status} - ${res.statusText}`);
+        }
+        return res;
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        nextStop = data.next;
+        currentStop = data.name;
+        info.textContent = `Next stop ${data.name}`;
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
+      })
+      .catch((err) => {
+        info.textContent = "Error";
+        departBtn.disabled = true;
+        arriveBtn.disabled = true;
+      });
+  }
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+  function arrive() {
+    info.textContent = `Arriving at ${currentStop}`;
+    currentStop = nextStop;
+    departBtn.disabled = false;
+    arriveBtn.disabled = true;
+  }
 
-    return {
-        depart,
-        arrive
-    };
+  return {
+    depart,
+    arrive,
+  };
 }
-
-let result = solve();
