af0218cfe77aa880d1dffa9f5abbc499676cdb91
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-04 22:42:11 +0300

    Update app.js
    BUS STOP FINISHED

--- REST Services and AJAX/01.BUS-STOP/app.js
+++ REST Services and AJAX/01.BUS-STOP/app.js
@@ -1,3 +1,21 @@
 function getInfo() {
-    console.log("TODO...");
+  const stopIdInput = document.getElementById("stopId");
+  const stopName = document.getElementById("stopName");
+  const busesList = document.getElementById("buses");
+  const url = `https://judgetests.firebaseio.com/businfo/${stopIdInput.value}.json `;
+
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      stopIdInput.value = "";
+      stopName.textContent = data.name;
+      Object.entries(data.buses).forEach((bus) => {
+        const li = document.createElement("li");
+        li.textContent = `Bus ${bus[0]} arrives in ${bus[1]}`;
+        busesList.appendChild(li);
+      });
+    })
+    .catch((err) => {
+      stopName.textContent = "Error";
+    });
 }
