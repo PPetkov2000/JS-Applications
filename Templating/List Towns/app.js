ebf0cb35221a306f62e350486b5e7449fcb4b249
PPetkov <ppetkov2000@abv.bg>
2020-07-13 17:10:52 +0300

    Templating initial setup

---
+++ Templating/List Towns/app.js
@@ -0,0 +1,8 @@
+const townsInput = document.getElementById("towns");
+const btnLoadTowns = document.getElementById("btnLoadTowns");
+
+function attachEvents() {
+  btnLoadTowns.addEventListener("click", loadTowns);
+
+  function loadTowns() {}
+}
