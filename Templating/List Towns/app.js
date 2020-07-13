497c579df9829a659d03d1b289dc39fed8d20f52
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-13 17:32:38 +0300

    Update app.js

--- Templating/List Towns/app.js
+++ Templating/List Towns/app.js
@@ -1,8 +1,22 @@
 const townsInput = document.getElementById("towns");
 const btnLoadTowns = document.getElementById("btnLoadTowns");
+const root = document.getElementById("root");
 
 function attachEvents() {
   btnLoadTowns.addEventListener("click", loadTowns);
 
-  function loadTowns() {}
+  async function loadTowns(e) {
+    e.preventDefault();
+
+    const towns = townsInput.value.split(", ");
+    const source = await fetch("./towns-template.hbs").then((res) =>
+      res.text()
+    );
+    const templateFn = Handlebars.compile(source);
+    const html = templateFn({ towns });
+
+    root.innerHTML = html;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", attachEvents);
