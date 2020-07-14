ae84983153cc30b003a7458b3648a996803d8406
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-14 20:39:51 +0300

    Update template.js

--- Templating/HTTP Status Cats/template.js
+++ Templating/HTTP Status Cats/template.js
@@ -1,8 +1,22 @@
 (() => {
-     renderCatTemplate();
+  renderCatTemplate();
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
-})
+  async function renderCatTemplate() {
+    const source = await fetch("./all-cats.hbs").then((res) => res.text());
+    const templateFn = Handlebars.compile(source);
+    document.getElementById("allCats").innerHTML = templateFn({ cats });
+
+    document.querySelectorAll(".showBtn").forEach((btn) => {
+      const status = btn.nextElementSibling;
+      btn.addEventListener("click", function () {
+        if (btn.textContent === "Show status code") {
+          btn.textContent = "Hide status code";
+          status.style.display = "block";
+        } else {
+          btn.textContent = "Show status code";
+          status.style.display = "none";
+        }
+      });
+    });
+  }
+})();
