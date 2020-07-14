9413d57c0be2ee180a436869be90613f1e7fd6ad
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-14 20:41:26 +0300

    Update monkeysTemplate.js

--- Templating/Popular Monkeys/monkeysTemplate.js
+++ Templating/Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,18 @@
-$(() => {
-    // TODO
-})
+async function getMonkeys() {
+  const source = await fetch("./monkeys.hbs").then((res) => res.text());
+  const templateFn = Handlebars.compile(source);
+  document.querySelector(".monkeys").innerHTML = templateFn({ monkeys });
+
+  document.querySelectorAll(".info").forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const info = btn.nextElementSibling;
+      if (info.style.display === "none") {
+        info.style.display = "block";
+      } else {
+        info.style.display = "none";
+      }
+    });
+  });
+}
+
+getMonkeys();
