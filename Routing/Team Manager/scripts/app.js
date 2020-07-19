f557ec7b157a89fc4e9149a420b2b332db34aa00
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-19 23:55:50 +0300

    add more routes

--- Routing/Team Manager/scripts/app.js
+++ Routing/Team Manager/scripts/app.js
@@ -8,9 +8,16 @@ const app = Sammy("#main", function () {
 
   this.get("#/register", controllers.user.get.register);
   this.get("#/login", controllers.user.get.login);
-  
+  this.get("#/logout", controllers.user.get.logout);
+
   this.post("#/register", controllers.user.post.register);
   this.post("#/login", controllers.user.post.login);
+
+  this.get("#/catalog", controllers.team.get.catalog);
+  this.get("#/catalog/:id", controllers.team.get.catalog);
+  this.get("#/create", controllers.team.get.create);
+
+  this.post("#/create", controllers.team.post.create);
 });
 
 app.run("#/home");
