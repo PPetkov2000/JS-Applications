bb8ecccc3eaa62d72d6c7f6aa9114105468ce0fd
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-22 15:37:04 +0300

    Update home.js

--- Routing/Team Manager/controllers/home.js
+++ Routing/Team Manager/controllers/home.js
@@ -1,11 +1,20 @@
 import extend from "../utils/context.js";
+import models from "../models/index.js";
 
 export default {
   get: {
     home(ctx) {
-      extend(ctx).then(function () {
-        this.partial("../templates/home/home.hbs");
-      });
+      models.team
+        .getUser("username", localStorage.getItem("username"))
+        .then((user) => {
+          ctx.hasTeam = user.docs.length > 0;
+          ctx.teamId = user.docs.map((doc) => doc.data().teamId)[0];
+
+          extend(ctx).then(function () {
+            this.partial("../templates/home/home.hbs");
+          });
+        })
+        .catch(console.error);
     },
     about(ctx) {
       extend(ctx).then(function () {
