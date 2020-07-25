a4b082c89767da53b0b3bb3a40eabfd08e3e683e
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-25 23:48:16 +0300

    Add files via upload

---
+++ Workshop - Single Page Application/config/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDsD_Am1u4mMUGq2G8k1a6cbpVtKMi_BU8",
+  authDomain: "movies-257d2.firebaseapp.com",
+  databaseURL: "https://movies-257d2.firebaseio.com",
+  projectId: "movies-257d2",
+  storageBucket: "movies-257d2.appspot.com",
+  messagingSenderId: "670734131073",
+  appId: "1:670734131073:web:9eb3b457fdf53af9075280",
+  measurementId: "G-SMPTF80FJ8",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
