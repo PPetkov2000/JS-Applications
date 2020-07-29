ecdd1975e75ead4521f313a43e440582c57985be
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-29 19:11:00 +0300

    Update notifications.js

--- Workshop - Single Page Application/scripts/notifications.js
+++ Workshop - Single Page Application/scripts/notifications.js
@@ -1,1 +1,22 @@
-notifications;
+const elements = {
+  info: document.getElementById("infoBox"),
+  error: document.getElementById("errorBox"),
+};
+
+function showElement(element, message) {
+  element.style.display = "block";
+  element.children[0].textContent = message;
+
+  setTimeout(function () {
+    element.style.display = "none";
+  }, 3000);
+}
+
+const showInfo = showElement.bind(undefined, elements.info);
+const showError = showElement.bind(undefined, elements.error);
+
+export default {
+  showInfo,
+  showError,
+};
+
