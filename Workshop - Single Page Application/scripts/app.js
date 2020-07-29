1ff3c73ec8ee905034fd35fbfb81a9f3031c4865
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-29 19:10:37 +0300

    Update app.js

--- Workshop - Single Page Application/scripts/app.js
+++ Workshop - Single Page Application/scripts/app.js
@@ -1,1 +1,32 @@
-app;
+import controllers from "../controllers/index.js";
+
+const app = Sammy("#container", function () {
+  this.use("Handlebars", "hbs");
+
+  this.get("#/home", controllers.home.get.home);
+
+  this.get("#/register", controllers.user.get.register);
+  this.get("#/login", controllers.user.get.login);
+  this.get("#/logout", controllers.user.get.logout);
+
+  this.post("#/register", controllers.user.post.register);
+  this.post("#/login", controllers.user.post.login);
+
+  this.get("#/cinema", controllers.movie.get.cinema);
+  this.get("#/create", controllers.movie.get.create);
+  this.get("#/profile", controllers.movie.get.profile);
+  this.get("#/details/:id", controllers.movie.get.details);
+  this.get("#/edit/:id", controllers.movie.get.edit);
+  this.get("#/delete/:id", controllers.movie.get.delete);
+  this.get("#/search", controllers.movie.get.search);
+
+  this.post("#/create", controllers.movie.post.create);
+
+  this.get("#/buyTicket/:id", controllers.movie.put.buyTicket);
+  this.post("#/edit/:id", controllers.movie.put.edit);
+
+  this.post("#/delete/:id", controllers.movie.delete.delete);
+});
+
+app.run("#/home");
+
