70bdc9648ced7ec6f589ab79d89661283356b400
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-07-31 17:06:16 +0300

    Update movie.js

--- Workshop - Single Page Application/models/movie.js
+++ Workshop - Single Page Application/models/movie.js
@@ -1,1 +1,24 @@
-movie;
+export default {
+  create(data) {
+    return firebase.firestore().collection("movies").add(data);
+  },
+  getAll() {
+    return firebase.firestore().collection("movies").get();
+  },
+  get(id) {
+    return firebase.firestore().collection("movies").doc(id).get();
+  },
+  update(id, data) {
+    return firebase.firestore().collection("movies").doc(id).update(data);
+  },
+  remove(id) {
+    return firebase.firestore().collection("movies").doc(id).delete();
+  },
+  getByCriteria(prop, value) {
+    return firebase
+      .firestore()
+      .collection("movies")
+      .where(prop, "==", value)
+      .get();
+  },
+};
