36b4af3172c3bd13a3d748b68259726249aa8a59
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-01 22:13:29 +0300

    add file structure

--- Workshop - Single Page Application/controllers/index.js
+++ Workshop - Single Page Application/controllers/index.js
@@ -1,1 +1,9 @@
-index;
+import home from "./controllers/home.js";
+import user from "./controllers/user.js";
+import movie from "./controllers/movie.js";
+
+export default {
+  home,
+  user,
+  movie
+}
