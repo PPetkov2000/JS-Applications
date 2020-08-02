b2ba48251bb09503f21af08abc384d10d5f5a5ed
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-02 20:56:33 +0300

    remove notifications logic

--- Workshop - Single Page Application/controllers/home.js
+++ Workshop - Single Page Application/controllers/home.js
@@ -3,9 +3,6 @@ import extend from "../utils/context.js";
 export default {
   get: {
     home(ctx) {
-      ctx.message = {
-        infoMessage: "User registration successful.",
-      };
       extend(ctx).then(function () {
         this.partial("../views/home/home.hbs");
       });
