909ab499f74e3aa24cd2448dec7ae240e1f8747e
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-02 20:58:39 +0300

    add user auth logic

--- Workshop - Single Page Application/controllers/user.js
+++ Workshop - Single Page Application/controllers/user.js
@@ -1,4 +1,59 @@
+import extend from "../utils/context.js";
+import models from "../models/index.js";
+import notifications from "../scripts/notifications.js";
+
 export default {
-  get: {},
-  post: {},
-}
+  get: {
+    register(ctx) {
+      extend(ctx).then(function () {
+        this.partial("../views/user/register.hbs");
+      });
+    },
+    login(ctx) {
+      extend(ctx).then(function () {
+        this.partial("../views/user/login.hbs");
+      });
+    },
+    logout(ctx) {
+      models.user
+        .logout()
+        .then(() => {
+          notifications.showInfo("Logout successful.");
+          ctx.redirect("#/home");
+        })
+        .catch((err) => {
+          notifications.showError(err.message);
+        });
+    },
+  },
+  post: {
+    register(ctx) {
+      const { username, password, repeatPassword } = ctx.params;
+
+      if (password === repeatPassword) {
+        models.user
+          .register(username, password)
+          .then(() => {
+            notifications.showInfo("User registration successful.");
+            ctx.redirect("#/home");
+          })
+          .catch((err) => {
+            notifications.showError(err.message);
+          });
+      }
+    },
+    login(ctx) {
+      const { username, password } = ctx.params;
+
+      models.user
+        .login(username, password)
+        .then(() => {
+          notifications.showInfo("Login successful.");
+          ctx.redirect("#/home");
+        })
+        .catch((err) => {
+          notifications.showError(err.message);
+        });
+    },
+  },
+};
