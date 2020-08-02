a3e01e7e7121d98bfcf41b362205228257728a93
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-02 20:54:46 +0300

    Update home.js

--- Workshop - Single Page Application/controllers/home.js
+++ Workshop - Single Page Application/controllers/home.js
@@ -1,4 +1,15 @@
+import extend from "../utils/context.js";
+
 export default {
-  get: {},
+  get: {
+    home(ctx) {
+      ctx.message = {
+        infoMessage: "User registration successful.",
+      };
+      extend(ctx).then(function () {
+        this.partial("../views/home/home.hbs");
+      });
+    },
+  },
   post: {},
-}
+};
