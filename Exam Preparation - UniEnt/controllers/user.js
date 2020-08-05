9c97d5a92be6f8a0ab62c534358918516470695f
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-05 15:16:45 +0300

    Update user.js

--- Exam Preparation - UniEnt/controllers/user.js
+++ Exam Preparation - UniEnt/controllers/user.js
@@ -1,4 +1,84 @@
+import extend from "../utils/context.js";
+import docModifier from "../utils/docModifier.js";
+import models from "../models/index.js";
+import notifications from "../scripts/notifications.js";
+
 export default {
-  get: {},
-  post: {}
+  get: {
+    register(ctx) {
+      extend(ctx).then(function () {
+        this.partial("../views/user/register.hbs");
+      });
+    },
+    login(ctx) {
+      extend(ctx).then(function () {
+        this.partial("../views/user/login.hbs");
+      });
+    },
+    logout(ctx) {
+      models.user
+        .logout()
+        .then(() => {
+          notifications.showSuccess("Logout successful.");
+          ctx.redirect("#/home");
+        })
+        .catch((err) => {
+          notifications.showError(err.message);
+        });
+    },
+    profile(ctx) {
+      const username = localStorage.getItem("username");
+
+      models.event
+        .getByCriteria("organizer", username)
+        .then((events) => {
+          const eventsData = events.docs.map(docModifier);
+          ctx.events = eventsData;
+          ctx.organizer = username;
+
+          extend(ctx).then(function () {
+            this.partial("../views/user/profile.hbs");
+          });
+        })
+        .catch((err) => {
+          notifications.showError(err.message);
+        });
+    },
+  },
+  post: {
+    register(ctx) {
+      const { username, password, rePassword } = ctx.params;
+
+      if (password !== rePassword) {
+        notifications.showError(
+          "The repeat password should be equal to the password"
+        );
+        return;
+      }
+
+      models.user
+        .register(username, password)
+        .then(() => {
+          notifications.showSuccess("User registration successful.");
+          ctx.redirect("#/home");
+        })
+        .catch((err) => {
+          notifications.showError(err.message);
+        });
+    },
+    login(ctx) {
+      const { username, password } = ctx.params;
+
+      models.user
+        .login(username, password)
+        .then(() => {
+          notifications.showSuccess("Login successful.");
+          ctx.redirect("#/home");
+        })
+        .catch((err) => {
+          notifications.showError(err.message);
+        });
+    },
+  },
 };
+
