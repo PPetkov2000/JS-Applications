0ff44a9fb712d52ba83e64cff1958d8064aa2e47
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-05 15:16:09 +0300

    Update home.js

--- Exam Preparation - UniEnt/controllers/home.js
+++ Exam Preparation - UniEnt/controllers/home.js
@@ -1,4 +1,28 @@
+import extend from "../utils/context.js";
+import docModifier from "../utils/docModifier.js";
+import models from "../models/index.js";
+import notifications from "../scripts/notifications.js";
+
 export default {
-  get: {},
-  post: {}
+  get: {
+    home(ctx) {
+      models.event
+        .getAll()
+        .then((events) => {
+          const eventsData = events.docs
+            .map(docModifier)
+            .sort((a, b) => b.peopleInterestedIn - a.peopleInterestedIn);
+          ctx.events = eventsData;
+
+          extend(ctx).then(function () {
+            this.partial("../views/home/home.hbs");
+          });
+        })
+        .catch((err) => {
+          notifications.showError(err.message);
+        });
+    },
+  },
+  post: {},
 };
+
