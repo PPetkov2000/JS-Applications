4e4cf656e704269ae15b0dc1cdcf6648d981b55d
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-07 12:09:02 +0300

    add firebase setup

---
+++ Exam Preparation - UniEnt/config/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAIHJyRkJuuRKdtdcvJzG1JBwRERi14o6o",
+  authDomain: "softterest-1018c.firebaseapp.com",
+  databaseURL: "https://softterest-1018c.firebaseio.com",
+  projectId: "softterest-1018c",
+  storageBucket: "softterest-1018c.appspot.com",
+  messagingSenderId: "647278999420",
+  appId: "1:647278999420:web:5feef7d05aa6c415ef96f9",
+  measurementId: "G-M7X9TX7QNB",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
