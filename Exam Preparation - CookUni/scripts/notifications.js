281a12dfeb51e393cba379d3ff5fd030757a575f
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-12 18:15:31 +0300

    Update notifications.js

--- Exam Preparation - CookUni/scripts/notifications.js
+++ Exam Preparation - CookUni/scripts/notifications.js
@@ -1,1 +1,22 @@
-Notifications;
+const elements = {
+  success: document.getElementById("successBox"),
+  error: document.getElementById("errorBox"),
+};
+
+function showElement(element, message) {
+  element.style.display = "block";
+  element.textContent = message;
+
+  setTimeout(function () {
+    element.style.display = "none";
+  }, 3000);
+}
+
+const showSuccess = showElement.bind(undefined, elements.success);
+const showError = showElement.bind(undefined, elements.error);
+
+export default {
+  showSuccess,
+  showError,
+};
+
