ec136a10dbdca0c5e3f96d15aa11a69ff2d7b6e8
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-13 14:42:18 +0300

    Update user.js

--- Exam Preparation - CookUni/models/user.js
+++ Exam Preparation - CookUni/models/user.js
@@ -1,1 +1,11 @@
-export default {};
+export default {
+  register(username, password) {
+    return firebase.auth().createUserWithEmailAndPassword(username, password);
+  },
+  login(username, password) {
+    return firebase.auth().signInWithEmailAndPassword(username, password);
+  },
+  logout() {
+    return firebase.auth().signOut();
+  },
+};
