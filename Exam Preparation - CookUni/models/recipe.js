a39a649332da74e9195258dae4a2af328ae9b113
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-13 14:42:36 +0300

    Update recipe.js

--- Exam Preparation - CookUni/models/recipe.js
+++ Exam Preparation - CookUni/models/recipe.js
@@ -1,1 +1,25 @@
-export default {};
+export default {
+  create(data) {
+    return firebase.firestore().collection("recipes").add(data);
+  },
+  getAll() {
+    return firebase.firestore().collection("recipes").get();
+  },
+  get(id) {
+    return firebase.firestore().collection("recipes").doc(id).get();
+  },
+  update(id, data) {
+    return firebase.firestore().collection("recipes").doc(id).update(data);
+  },
+  remove(id) {
+    return firebase.firestore().collection("recipes").doc(id).delete();
+  },
+  getByCriteria(prop, value) {
+    return firebase
+      .firestore()
+      .collection("recipes")
+      .where(prop, "==", value)
+      .get();
+  },
+};
+
