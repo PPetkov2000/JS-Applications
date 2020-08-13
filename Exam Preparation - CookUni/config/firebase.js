3e2152247b1dfeada1a8f6e297ffc0f3c2088f9e
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-13 14:39:35 +0300

    add firebase config

---
+++ Exam Preparation - CookUni/config/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAIHJyRkJuuRKdtdcvJzG1JBwRERi14o6o",
+  authDomain: "softterest-1018c.firebaseapp.com",
+  databaseURL: "https://softterest-1018c.firebaseio.com",
+  projectId: "softterest-1018c",
+  storageBucket: "softterest-1018c.appspot.com",
+  messagingSenderId: "647278999420",
+  appId: "1:647278999420:web:db9c99580a1c8151ef96f9",
+  measurementId: "G-KR7S4M69E7",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
