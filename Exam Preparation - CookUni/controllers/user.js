36eeaadd7df3a9339d92d18a9e2139a587ae8b95
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-14 16:25:55 +0300

    Update user.js

--- Exam Preparation - CookUni/controllers/user.js
+++ Exam Preparation - CookUni/controllers/user.js
@@ -1,1 +1,66 @@
-export default {};
+import extend from "../utils/context.js";
+import models from "../models/index.js";
+import notifications from "../scripts/notifications.js";
+
+export default {
+  get: {
+    register(ctx) {
+      extend(ctx).then(function () {
+        this.partial("../views/user/register.hbs");
+      });
+    },
+    login(ctx) {
+      extend(ctx).then(function () {
+        this.partial("../views/user/login.hbs");
+      });
+    },
+    logout(ctx) {
+      models.user
+        .logout()
+        .then(() => {
+          notifications.showSuccess("Logout successful.");
+          ctx.redirect("#/home");
+        })
+        .catch((err) => {
+          notifications.showError(err.message);
+        });
+    },
+  },
+  post: {
+    register(ctx) {
+      const {
+        firstName,
+        lastName,
+        username,
+        password,
+        repeatPassword,
+      } = ctx.params;
+
+      if (password === repeatPassword) {
+        models.user
+          .register(username, password)
+          .then(() => {
+            notifications.showSuccess("User registration successful.");
+            ctx.redirect("#/home");
+          })
+          .catch((err) => {
+            notifications.showError(err.message);
+          });
+      }
+    },
+    login(ctx) {
+      const { username, password } = ctx.params;
+
+      models.user
+        .login(username, password)
+        .then(() => {
+          notifications.showSuccess("Login successful.");
+          ctx.redirect("#/home");
+        })
+        .catch((err) => {
+          notifications.showError(err.message);
+        });
+    },
+  },
+};
+
