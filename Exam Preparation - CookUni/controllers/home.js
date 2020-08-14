9d1f237c8b9dd7c6dd7f797f8bf2ee760be5067d
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-14 16:25:27 +0300

    Update home.js

--- Exam Preparation - CookUni/controllers/home.js
+++ Exam Preparation - CookUni/controllers/home.js
@@ -1,1 +1,26 @@
-export default {};
+import extend from "../utils/context.js";
+import docModifier from "../utils/docModifier.js";
+import models from "../models/index.js";
+import notifications from "../scripts/notifications.js";
+
+export default {
+  get: {
+    home(ctx) {
+      models.recipe
+        .getAll()
+        .then((recipes) => {
+          const recipesData = recipes.docs.map(docModifier);
+          ctx.recipes = recipesData;
+
+          extend(ctx).then(function () {
+            this.partial("../views/home/home.hbs");
+          });
+        })
+        .catch((err) => {
+          notifications.showError(err.message);
+        });
+    },
+  },
+  post: {},
+};
+
